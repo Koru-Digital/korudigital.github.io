274f11bd9cfe09a290d7b3dda7677ebf92785732
Glenn Barr <glenn.barr@focusrite.com>
2021-08-12 14:18:32 +0100

    Revert deployment for now

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,16 +1,17 @@
-var ghpages = require('gh-pages');
+import { publish } from 'gh-pages';
 
-ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/korudigital/korudigital.github.io.git', // Update to point to your repository
-        user: {
-            name: 'kiwimind', // update to use your name
-            email: 'glenn@dudeshouse.com' // Update to use your email
-        }
+publish(
+  'build', // path to public directory
+  {
+    branch: 'gh-pages',
+    repo: 'https://github.com/korudigital/korudigital.github.io.git', // Update to point to your repository
+    user: {
+      name: 'kiwimind', // update to use your name
+      email: 'glenn@dudeshouse.com' // Update to use your email
     },
-    () => {
-        console.log('Deploy Complete!')
-    }
-)
+    dotfiles: true
+  },
+  () => {
+    console.log('Deploy Complete!');
+  }
+);
