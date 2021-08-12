c439776734ff18bedcccc796b13187a991c65b5d
Glenn Barr <glenn.barr@focusrite.com>
2021-08-12 15:26:21 +0100

    Use correct build directory

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -10,7 +10,7 @@ const fs = require("fs");
     console.log("Building...");
     await execa("npm", ["run", "build"]);
     // Understand if it's dist or build folder
-    const folderName = "public";
+    const folderName = "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
