90c18ac74d8ff884a63130ca9e758521127096a7
Glenn Barr <glenn.barr@focusrite.com>
2021-08-12 15:24:31 +0100

    Import require

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,23 +1,26 @@
-const execa = require("execa");
-  const fs = require("fs");
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-  (async () => {
-    try {
-      await execa("git", ["checkout", "--orphan", "gh-pages"]);
-      console.log("Building...");
-      await execa("npm", ["run", "build"]);
-      // Understand if it's dist or build folder
-      const folderName = "public";
-      await execa("git", ["--work-tree", folderName, "add", "--all"]);
-      await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-      console.log("Pushing to gh-pages...");
-      await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-      await execa("rm", ["-r", folderName]);
-      await execa("git", ["checkout", "-f", "main"]);
-      await execa("git", ["branch", "-D", "gh-pages"]);
-      console.log("Successfully deployed");
-    } catch (e) {
-      console.log(e.message);
-      process.exit(1);
-    }
-  })();
+const execa = require("execa");
+const fs = require("fs");
+
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    console.log("Building...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = "public";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-r", folderName]);
+    await execa("git", ["checkout", "-f", "main"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed");
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
