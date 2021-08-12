ede24ed63bc826531c0e4ea8cc0b5bd7ce8578cb
Glenn Barr <glenn.barr@focusrite.com>
2021-08-12 14:25:15 +0100

    Try new config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
+
 	kit: {
+		target: '#svelte',
 		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
+			pages: 'build',  // path to public directory
+			assets: 'build',  // path to public directory
 			fallback: null
 		})
 	}
